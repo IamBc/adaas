b69218c6a76fd87209385545ee2ad2bb76c46b74
Bloodcount <searosss@gmail.com>
2016-03-20 22:06:14 +0200

    feat: envvar for path to web_ui files

--- run_server.sh
+++ run_server.sh
@@ -1,6 +1,13 @@
 echo "Starting server"
+
+#Env setup
 export UPLOAD_DATASET_FILE_DIR="/tmp/"
+export WEBUI_DIR="" 
+
+#Build
 cd server
 /usr/local/go/bin/go build 
+
+#Run
 cd ..
 ./server/server  -logtostderr=true
