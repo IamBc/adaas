aff05fb6baf5effbc2a7d7a6d704e83cd8061534
Bloodcount <searosss@gmail.com>
2016-03-20 22:30:20 +0200

    feat: relative path from the project

--- run_server.sh
+++ run_server.sh
@@ -1,13 +1,15 @@
-echo "Starting server"
 
 #Env setup
+echo 'Setting the envornment...'
 export UPLOAD_DATASET_FILE_DIR="/tmp/"
-export WEBUI_DIR="" 
+export WEBUI_DIR="./web-client" 
 
 #Build
+echo 'Building the project...'
 cd server
 /usr/local/go/bin/go build 
 
 #Run
+echo 'Running the project...'
 cd ..
 ./server/server  -logtostderr=true
